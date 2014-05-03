93bebc44e2a82a3e57ef4071657abafdf560bf59
fripig <fripig@gmail.com>
2014-05-04 02:20:42 +0800

    修正

--- js/installed_remove.js
+++ js/installed_remove.js
@@ -2,4 +2,9 @@
 just hide install btn
 */
 installbtn = document.querySelector(".install_extension");
-installbtn.style.display ="none";
+console.log(installbtn);
+for(var key in installbtn)
+{
+  target = installbtn[key];
+  installbtn.style.display ="none";
+}
